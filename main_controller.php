99410df08d7e6fbb314dade1a60bc5d89078d8d3
Vinusha Perera <vinushaperera357@gmail.com>
2018-12-11 08:32:59 +0530

    Controller

--- main_controller.php
+++ main_controller.php
@@ -7,8 +7,11 @@ function rateReview(){
     $url = 'http://127.0.0.1:5000/sentimentPred';
     $ch = curl_init($url);
 
+    $data = array("Text" => [$GLOBALS['review']]);                                                                    
+    $data_string = json_encode($data);
+
     curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $GLOBALS['review']);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
     $response = curl_exec($ch);
