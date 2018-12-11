f7931c554f38e305ab7f79db88cffa40d5d6a4df
Vinusha Perera <vinushaperera357@gmail.com>
2018-12-11 07:44:11 +0530

    Rate review

--- db_handler.php
+++ db_handler.php
@@ -2,6 +2,8 @@
 
 include 'db_access.php';
 
-
+function updateReview(){
+    
+}
 
 ?>
--- main_controller.php
+++ main_controller.php
@@ -3,7 +3,18 @@
 include 'db_handler.php';
 
 function rateReview(){
-    header('Location: ' . $GLOBALS['movie_id'] . '|' . $GLOBALS['review'] . '.php');
+
+    $url = 'http://127.0.0.1:5000/sentimentPred';
+    $ch = curl_init($url);
+
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $GLOBALS['review']);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    header('Location: ' . $response . '.php');
 }
 
 ?>
