e9da936e9780585ecdf1d579758d827a5da37e1f
Vinusha Perera <vinushaperera357@gmail.com>
2018-12-11 09:22:27 +0530

    request changes

--- main_controller.php
+++ main_controller.php
@@ -7,18 +7,18 @@ function rateReview(){
     $url = 'http://127.0.0.1:5000/sentimentPred';
     $ch = curl_init($url);
 
-    $data = array("Text" => [$GLOBALS['review']]);                                                                    
+    $data = array("Text" => array($GLOBALS['review']));                                                                    
     $data_string = json_encode($data);
 
     curl_setopt($ch, CURLOPT_POST, 1);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
     curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
     $response = curl_exec($ch);
     curl_close($ch);
 
-    echo "<pre>$result</pre>";
+    echo "<pre>$response</pre>";
 }
 
 ?>
